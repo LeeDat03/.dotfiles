3dac9e64fb2c528e3f8387674d64d005b8f1ae2c
LeeDat03 <leedat104517@gmail.com>
2025-09-07 15:17:27 +0700

    change statusline

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -30,49 +30,146 @@ return {
 			},
 		},
 	},
-
-	-- Statusline
 	{
-		"echasnovski/mini.statusline",
-		version = "*",
-		event = "BufReadPost",
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
 		config = function()
-			local MiniStatusline = require("mini.statusline")
-
-			local function custom_section_location()
-				return "%2l│%-2v" -- Always show "line│column"
-			end
-
-			MiniStatusline.setup({
-				content = {
-					active = function()
-						local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-						local git = MiniStatusline.section_git({ trunc_width = 40 })
-						local diff = MiniStatusline.section_diff({ trunc_width = 75 })
-						local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-						local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
-
-						local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
-						local fileinfo = vim.bo.filetype ~= "" and vim.bo.filetype:lower() or ""
-
-						local location = custom_section_location()
-						local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
-
-						return MiniStatusline.combine_groups({
-							{ hl = mode_hl, strings = { mode } },
-							{ hl = "MiniStatuslineDevinfo", strings = { git } },
-							{ hl = "MiniStatuslineDevinfo", strings = { diff } },
-							{ hl = "MiniStatuslineDevinfo", strings = { diagnostics } },
-
-							"%<", -- Mark general truncate point
-							{ hl = "MiniStatuslineFilename", strings = { filename } },
-							"%=", -- End left alignment
-							{ hl = "MiniStatuslineFilename", strings = { fileinfo } },
-							{ hl = "MiniStatuslineDevinfo", strings = { lsp } },
-							{ hl = mode_hl, strings = { search, location } },
-						})
-					end,
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "auto",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = { "NvimTree", "TelescopePrompt" },
+					always_divide_middle = true,
+					always_show_tabline = false,
+					globalstatus = false,
 				},
+				sections = {
+					lualine_a = {
+						{
+							function()
+								local mode = vim.api.nvim_get_mode().mode
+								local mode_map = {
+									n = "N",
+									no = "N",
+									nov = "N",
+									niI = "N",
+									niR = "N",
+									niV = "N",
+									nt = "N",
+									v = "V",
+									vs = "V",
+									V = "V",
+									Vs = "V",
+									[""] = "V",
+									s = "S",
+									S = "S",
+									[""] = "S",
+									i = "I",
+									ic = "I",
+									ix = "I",
+									R = "R",
+									Rc = "R",
+									Rx = "R",
+									Rv = "R",
+									c = "C",
+									cv = "C",
+									ce = "C",
+									r = "R",
+									rm = "M",
+									["r?"] = "?",
+									["!"] = "!",
+									t = "T",
+								}
+								return mode_map[mode] or mode:sub(1, 1):upper()
+							end,
+							color = { gui = "bold" },
+						},
+					},
+
+					lualine_b = {},
+
+					lualine_c = {
+						{
+							function()
+								local filename = vim.fn.expand("%:t")
+								local file_status = ""
+								if vim.bo.modified then
+									file_status = file_status .. " ●"
+								end
+								if vim.bo.readonly then
+									file_status = file_status .. " "
+								end
+								if filename == "" then
+									return "[No Name]"
+								end
+
+								return filename .. file_status
+							end,
+							icon = "",
+						},
+					},
+
+					lualine_x = {
+						{
+							"diff",
+							cond = function()
+								return vim.fn.winwidth(0) > 40
+							end,
+						},
+						{
+							"diagnostics",
+							cond = function()
+								return vim.fn.winwidth(0) > 40
+							end,
+						},
+						{
+							function()
+								local filename = vim.fn.expand("%:t")
+								local extension = vim.fn.expand("%:e")
+								local icon = require("nvim-web-devicons").get_icon(
+									filename,
+									extension,
+									{ default = true }
+								) or ""
+
+								local bufnr = vim.api.nvim_get_current_buf()
+								local clients = vim.lsp.get_clients({ bufnr = bufnr })
+
+								if #clients == 0 then
+									return icon .. " no LSP"
+								end
+								return icon .. " LSP"
+							end,
+							cond = function()
+								return vim.fn.winwidth(0) > 60
+							end,
+						},
+					},
+					lualine_y = {},
+					lualine_z = { "branch" },
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { "filename" },
+					lualine_x = { "progress" },
+					lualine_y = {},
+					lualine_z = {},
+				},
+
+				tabline = {},
+				winbar = {},
+				inactive_winbar = {},
+				extensions = {},
 			})
 		end,
 	},
